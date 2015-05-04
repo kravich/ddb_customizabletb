d4de2fad36039354383b5a1f9f5ca1a4029c8f14
Evgeny Kravchenko <cravchik@yandex.ru>
2015-05-04 20:41:13 +1000

    Implemented activation of legacy actions

--- w_toolbar.c
+++ w_toolbar.c
@@ -116,6 +116,44 @@ void w_toolbar_save(ddb_gtkui_widget_t *w, char *s, int sz)
     g_free(layout_param);
 }
 
+/* This function was copied from Deadbeef sources */
+void activate_action_14(DB_plugin_action_t *action, int cursor)
+{
+    // Plugin can handle all tracks by itself
+    if (action->flags & DB_ACTION_CAN_MULTIPLE_TRACKS)
+    {
+        action->callback (action, NULL);
+        return;
+    }
+
+    // For single-track actions just invoke it with first selected track
+    if (!(action->flags & DB_ACTION_MULTIPLE_TRACKS))
+    {
+        if (cursor == -1) {
+            cursor = deadbeef->pl_get_cursor (PL_MAIN);
+        }
+        if (cursor == -1)
+        {
+            return;
+        }
+        DB_playItem_t *it = deadbeef->pl_get_for_idx_and_iter (cursor, PL_MAIN);
+        action->callback (action, it);
+        deadbeef->pl_item_unref (it);
+        return;
+    }
+
+    //We end up here if plugin won't traverse tracks and we have to do it ourselves
+    DB_playItem_t *it = deadbeef->pl_get_first (PL_MAIN);
+    while (it) {
+        if (deadbeef->pl_is_selected (it)) {
+            action->callback (action, it);
+        }
+        DB_playItem_t *next = deadbeef->pl_get_next (it, PL_MAIN);
+        deadbeef->pl_item_unref (it);
+        it = next;
+    }
+}
+
 void toolbar_button_activate_action(GtkButton *button, gpointer user_data)
 {
     ToolbarItem *item = (ToolbarItem*)user_data;
@@ -124,7 +162,9 @@ void toolbar_button_activate_action(GtkButton *button, gpointer user_data)
 
     printf("Activating \"%s\" in context %d\n", item->action_name, item->action_context);
 
-    if(item->action->callback2)
+    if(item->action->callback)
+        activate_action_14(item->action, -1);
+    else if(item->action->callback2)
         item->action->callback2(item->action, item->action_context);
 }
 
