9dc65cd67e669ee1cbec70a0238faa0ada4d24d5
Evgeny Kravchenko <cravchik@yandex.ru>
2015-05-17 18:39:27 +1000

    Added header to source files with copyright notice and file with the text of the license

--- customization_dialog.c
+++ customization_dialog.c
@@ -1,3 +1,23 @@
+/*
+    ddb_customizabletb - Customizable toolbar plugin for DeaDBeeF Music player
+    Copyright (C) 2015 Evgeny Kravchenko <cravchik@yandex.ru>
+
+    This file is part of ddb_customizabletb.
+
+    ddb_customizabletb is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ddb_customizabletb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ddb_customizabletb.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <gtk/gtk.h>
 
 #include <stdlib.h>
